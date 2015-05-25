6f4960a9ce46f46c01ed2b05c51f4474934fdc64
wuyang <wuyang910217@gmail.com>
2015-05-25 21:31:08 +0800

    add footer of homepage and some css style

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,13 +1,18 @@
 class HomepageController < ApplicationController
 	layout "homepage"
   def welcome
-  	dayOfWeek = [ "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" ]
-  	time=Time.now
-  	@current_time=time.strftime("%Y年%m月%d日 ") +   dayOfWeek[time.strftime("%w").to_i] +  time.strftime(" %H:%M %p")
+    $blog_time=144
+    dayOfWeek = [ "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" ]
+    time=Time.now
+    @current_time=time.strftime("%Y年%m月%d日 ") +   dayOfWeek[time.strftime("%w").to_i] +  time.strftime(" %H:%M:%S") 
+    @blog_run_time=time.strftime("%j").to_i - $blog_time
+
   end
   def about
   end
   def help
   end
+
+
 
 end
