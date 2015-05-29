a4d78f08ad6e36cf60717cf615da48b188b8036e
wuyang <wuyang910217@gmail.com>
2015-05-29 20:38:20 +0800

    add flash and cookies and i18n supports

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def signup
   	@user=User.new
   end
@@ -9,27 +10,36 @@ class UsersController < ApplicationController
   def create_login_session
   	user=User.find_by_name(params[:name])
   	if user && user.authenticate(params[:password])
-  		session[:user_id]=user_id
+  		if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
+      flash.notice ="登录成功！"
   		redirect_to :index
   	else
+      flash.notice ="用户名密码错误！"
   		redirect_to :login
   	end
   end
 
   def logout
-  	session[:user_id] = nil
+  	cookies.delete(:auth_token)
   	redirect_to :index
+  end
 
   def create
-  	user=User.new(user_params)
-  	user.save
-  	redirect_to :index
+  	@user=User.new(user_params)
+  	if user.save
+      cookies[:auth_token] = @user.auth_token
+  	  redirect_to :index
+    else
+      render :signup
   end
 
   private
   def user_params
   	params.require(:user).permit!
   end
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528135912) do
+ActiveRecord::Schema.define(version: 20150529110644) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150528135912) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "auth_token"
   end
 
 end
